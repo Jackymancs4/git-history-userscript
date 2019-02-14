9f3303d36ddf8e5bd6c6efa9e49d9b1f68d68c80
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:15:59 +0100

    Create git-history.userscript.js

---
+++ git-history.userscript.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name            Github Git History
+// @namespace       jackymancs4/userscripts
+// @version         1.0.0
+// @description     Add a button to the file interface to see its history
+// @author          Giacomo Rossetto <jackyman_cs4@live.it>
+// @match           https://github.com/*
+// @updateURL       https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/git-history.userscript.js
+// @downloadURL     https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/git-history.userscript.js
+// @require         https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/github-git-history.userscript.js
+// @grant           GM_log
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
