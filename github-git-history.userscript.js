2890ac7012ffbb9bbc3d22dac4b805f53067dd35
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:15:55 +0100

    Fix header indentation on github userscript

--- github-git-history.userscript.js
+++ github-git-history.userscript.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         Github Git History
-// @namespace    jackymancs4/userscripts
-// @version      1.0.0
-// @description  Add a button to the github file interface to see its history
-// @author       Giacomo Rossetto <jackyman_cs4@live.it>
-// @match        https://github.com/*
-// @updateURL   https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/github-git-history.userscript.js
-// @downloadURL https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/github-git-history.userscript.js
+// @name            Github Git History
+// @namespace       jackymancs4/userscripts
+// @version         1.0.1
+// @description     Add a button to the github file interface to see its history
+// @author          Giacomo Rossetto <jackyman_cs4@live.it>
+// @match           https://github.com/*
+// @updateURL       https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/github-git-history.userscript.js
+// @downloadURL     https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/github-git-history.userscript.js
 // ==/UserScript==
 
 (function() {
