bd64f0786876bd8630f793e0e5c6ddaba739df13
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:24:05 +0100

    Add gitlab support

--- git-history.userscript.js
+++ git-history.userscript.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name            Git History
 // @namespace       jackymancs4/userscripts
-// @version         1.0.0
+// @version         2.0.0
 // @description     Add a button to the file interface to see its history
 // @author          Giacomo Rossetto <jackyman_cs4@live.it>
 // @match           https://github.com/*
+// @match           https://gitlab.com/*
 // @updateURL       https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/git-history.userscript.js
 // @downloadURL     https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/git-history.userscript.js
 // @require         https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/github-git-history.userscript.js
+// @require         https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/gitlab-git-history.userscript.js
 // @grant           GM_log
 // ==/UserScript==
 
