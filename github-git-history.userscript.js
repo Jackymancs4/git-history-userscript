a1ca8372539bfc886a8b951f0da39992154b3ed2
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:04:34 +0100

    Create github-git-history.userscript.js

---
+++ github-git-history.userscript.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Github Git History
+// @namespace    jackymancs4/userscripts
+// @version      1.0.0
+// @description  Add a button to the github file interface to see its history
+// @author       Giacomo Rossetto <jackyman_cs4@live.it>
+// @match        https://github.com/*
+// @updateURL   https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/github-git-history.userscript.js
+// @downloadURL https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/github-git-history.userscript.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let url = window.location.href;
+    const auxUrl = url.split('/');
+    auxUrl[2] = 'github.githistory.xyz'
+    url = auxUrl.join('/');
+
+    const buttonGithubHistory = document.createElement('a');
+    buttonGithubHistory.innerHTML = 'Open in Git History';
+    buttonGithubHistory.setAttribute('class', 'btn btn-sm BtnGroup-item');
+    buttonGithubHistory.setAttribute('href', url);
+    document.getElementById("raw-url").parentNode.appendChild(buttonGithubHistory);
+})();
