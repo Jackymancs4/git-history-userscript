2455544062c8e6efa804843f047f74f49a8476c6
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:35:38 +0100

    Add bitbucket support

--- git-history.userscript.js
+++ git-history.userscript.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name            Git History
 // @namespace       jackymancs4/userscripts
-// @version         2.0.0
+// @version         3.0.0
 // @description     Add a button to the file interface to see its history
 // @author          Giacomo Rossetto <jackyman_cs4@live.it>
 // @match           https://github.com/*
 // @match           https://gitlab.com/*
+// @match           https://bitbucket.org/*
 // @updateURL       https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/git-history.userscript.js
 // @downloadURL     https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/git-history.userscript.js
 // @require         https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/github-git-history.userscript.js
 // @require         https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/gitlab-git-history.userscript.js
+// @require         https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/bitbucket-git-history.userscript.js
 // @grant           GM_log
 // ==/UserScript==
 
