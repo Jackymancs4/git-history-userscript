d5d5119b74660a01dc4f331d54ccd4e3867e3523
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:36:20 +0100

    Add initial URL check

--- github-git-history.userscript.js
+++ github-git-history.userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Github Git History
 // @namespace       jackymancs4/userscripts
-// @version         1.0.1
+// @version         1.1.0
 // @description     Add a button to the github file interface to see its history
 // @author          Giacomo Rossetto <jackyman_cs4@live.it>
 // @match           https://github.com/*
@@ -9,17 +9,21 @@
 // @downloadURL     https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/github-git-history.userscript.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     let url = window.location.href;
-    const auxUrl = url.split('/');
-    auxUrl[2] = 'github.githistory.xyz'
-    url = auxUrl.join('/');
+    let parsedURL = new URL(url)
 
-    const buttonGithubHistory = document.createElement('a');
-    buttonGithubHistory.innerHTML = 'Open in Git History';
-    buttonGithubHistory.setAttribute('class', 'btn btn-sm BtnGroup-item');
-    buttonGithubHistory.setAttribute('href', url);
-    document.getElementById("raw-url").parentNode.appendChild(buttonGithubHistory);
+    if (parsedURL.host == "github.com") {
+        const auxUrl = url.split('/');
+        auxUrl[2] = 'github.githistory.xyz'
+        url = auxUrl.join('/');
+
+        const buttonGithubHistory = document.createElement('a');
+        buttonGithubHistory.innerHTML = 'Open in Git History';
+        buttonGithubHistory.setAttribute('class', 'btn btn-sm BtnGroup-item');
+        buttonGithubHistory.setAttribute('href', url);
+        document.getElementById("raw-url").parentNode.appendChild(buttonGithubHistory);
+    }
 })();
