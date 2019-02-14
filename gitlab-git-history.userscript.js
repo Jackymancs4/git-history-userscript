ba32c41f48f47202274f309cbe14a4df79c21ede
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:15:26 +0100

    Create gitlab-git-history.userscript.js

---
+++ gitlab-git-history.userscript.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name            Gitlab Git History
+// @namespace       jackymancs4/userscripts
+// @version         1.0.0
+// @description     Add a button to the gitlab file interface to see its history
+// @author          Giacomo Rossetto <jackyman_cs4@live.it>
+// @match           https://github.com/*
+// @updateURL       https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/gitlab-git-history.userscript.js
+// @downloadURL     https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/gitlab-git-history.userscript.js
+// @grant           GM_log
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_log("Not implemented yet! Sorry.")
+
+})();
