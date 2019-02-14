2fd167d63a547db8c601c96aa630926b7b872be1
Giacomo Rossetto <jackyman_cs4@live.it>
2019-02-14 20:23:55 +0100

    Fix gitlab match permission

--- gitlab-git-history.userscript.js
+++ gitlab-git-history.userscript.js
@@ -4,7 +4,7 @@
 // @version         1.0.0
 // @description     Add a button to the gitlab file interface to see its history
 // @author          Giacomo Rossetto <jackyman_cs4@live.it>
-// @match           https://github.com/*
+// @match           https://gitlab.com/*
 // @updateURL       https://raw.githubusercontent.com/Jackymancs4/git-history-userscript/master/gitlab-git-history.userscript.js
 // @downloadURL     https://raw.githubusercontent.com/Jackymancs4/git-history-userscripts/master/gitlab-git-history.userscript.js
 // @grant           GM_log
